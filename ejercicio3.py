d6fc23119dbdc7bb7f03c6623e60421cf93aa067
Franco Passeggi <francopasseggi@gmail.com>
2022-11-08 11:23:04 -0300

    fix: the diff between codes must be symmetric

--- ejercicio3.py
+++ ejercicio3.py
@@ -6,7 +6,7 @@ def validate_discount_code(discount_code):
     discount_code = set(discount_code)
 
     for code in _AVAILABLE_DISCOUNT_CODES:
-        diff = set(code) - discount_code
+        diff = set(code) ^ discount_code  # ^ is the symmetric difference operator
 
         if len(diff) <= 2:
             return True
